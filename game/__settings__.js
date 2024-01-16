a5159501eb334f0504e382f331b6be37ece5409c
Buss-beep-3 <151694239+Buss-beep-3@users.noreply.github.com>
2024-01-15 18:48:17 -0600

    Add files via upload

---
+++ game/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1926256.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 162338321, 162338322, 162339716, 162349364, 162358744, 162361067, 162361068, 162383173 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/162338326/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/162338323/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/162338325/1/ammo.js', 'preload' : true},
+];
